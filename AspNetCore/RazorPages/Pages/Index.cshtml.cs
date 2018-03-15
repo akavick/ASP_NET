ab9dba1afc224deeb990d44e5cc3c78bbc908f54
akavick <akavick@tut.by>
2018-03-16 01:05:16 +0300

    1

--- AspNetCore/RazorPages/Pages/Index.cshtml.cs
+++ AspNetCore/RazorPages/Pages/Index.cshtml.cs
@@ -7,26 +7,53 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPages.Classes;
 using RazorPages.Repositories;
-using Syncfusion.EJ2.Grids;
+using Newtonsoft;
 
 namespace RazorPages.Pages
 {
-    public class AnotherModer : PageModel
-    {
-        public int Id { get; set; }
-    }
-
-
-
-
     public class IndexModel : PageModel
     {
         public List<Record> Records { get; private set; }
 
-
         public void OnGet()
         {
             Records = Repository.Records;
+        }
+
+        public IActionResult UrlDatasource([FromBody]Data dm)
+        {
+            if (order.Count() == 0)
+                BindDataSource();
+            var DataSource = order.ToList();
+            int count = order.Count();
+            var result = new DataResult
+            {
+                result = DataSource.Skip(dm.skip).Take(dm.take).ToList(),
+                count = count
+            };
+            return dm.requiresCounts ? Json(new { result = Data.Skip(dm.skip).Take(dm.take), count = count }) : Json(Data);
+        }
+
+        public class DataResult
+        {
+            public List<EditableOrder> result { get; set; }
+            public int count { get; set; }
+        }
+
+        public ActionResult CrudUpdate([FromBody]EditableOrder value, string action)
+        {
+            if (action == "update")
+            {
+                //Update record in database
+            }
+            else if (action == "insert")
+            {
+                //Insert record in database
+            }
+            else
+            {
+                //Delete record in database
+            }
         }
     }
 }
