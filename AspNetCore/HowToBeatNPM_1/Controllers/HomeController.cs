674aee06f9b79ae45009d8f740c6565ae6799a90
akavick <akavick@tut.by>
2018-03-16 16:18:39 +0300

    1

--- AspNetCore/HowToBeatNPM_1/Controllers/HomeController.cs
+++ AspNetCore/HowToBeatNPM_1/Controllers/HomeController.cs
@@ -20,6 +20,73 @@ namespace HowToBeatNPM_1.Controllers
             return View();
         }
 
+
+
+
+
+
+
+
+
+        public IActionResult UrlDatasource([FromBody]dynamic dm)
+        {
+            var dataSource = Repository.Records;
+            int count = dataSource.Count;
+
+            return (bool)dm.requiresCounts ? Json(new { result = dataSource.Skip((int)dm.skip).Take((int)dm.take).ToList(), count = count }) : Json(dataSource);
+        }
+
+        public class DataResult
+        {
+            public List<Record> Result { get; set; }
+            public int Count { get; set; }
+        }
+
+
+        public ActionResult Insert([FromBody]Record value)
+        {
+            return Json(value);
+        }
+        public ActionResult Update([FromBody]Record value)
+        {
+            return Json(value);
+        }
+        public ActionResult Delete([FromBody]Record value)
+        {
+            return Json(value);
+        }
+
+
+        public ActionResult CrudUpdate([FromBody]dynamic value, string action)
+        {
+            //if (action == "update")
+            //{
+            //    //Update record in database
+            //}
+            //else if (action == "insert")
+            //{
+            //    //Insert record in database
+            //}
+            //else
+            //{
+            //    //Delete record in database
+            //}
+
+            //return Json(value, JsonRequestBehavior.AllowGet);
+
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- AspNetCore/RazorPages/Pages/Index.cshtml.cs
+++ AspNetCore/RazorPages/Pages/Index.cshtml.cs
@@ -20,40 +20,40 @@ namespace RazorPages.Pages
             Records = Repository.Records;
         }
 
-        public IActionResult UrlDatasource([FromBody]Data dm)
-        {
-            if (order.Count() == 0)
-                BindDataSource();
-            var DataSource = order.ToList();
-            int count = order.Count();
-            var result = new DataResult
-            {
-                result = DataSource.Skip(dm.skip).Take(dm.take).ToList(),
-                count = count
-            };
-            return dm.requiresCounts ? Json(new { result = Data.Skip(dm.skip).Take(dm.take), count = count }) : Json(Data);
-        }
+        //public IActionResult UrlDatasource([FromBody]Data dm)
+        //{
+        //    if (order.Count() == 0)
+        //        BindDataSource();
+        //    var DataSource = order.ToList();
+        //    int count = order.Count();
+        //    var result = new DataResult
+        //    {
+        //        result = DataSource.Skip(dm.skip).Take(dm.take).ToList(),
+        //        count = count
+        //    };
+        //    return dm.requiresCounts ? Json(new { result = Data.Skip(dm.skip).Take(dm.take), count = count }) : Json(Data);
+        //}
 
-        public class DataResult
-        {
-            public List<EditableOrder> result { get; set; }
-            public int count { get; set; }
-        }
+        //public class DataResult
+        //{
+        //    public List<EditableOrder> result { get; set; }
+        //    public int count { get; set; }
+        //}
 
-        public ActionResult CrudUpdate([FromBody]EditableOrder value, string action)
-        {
-            if (action == "update")
-            {
-                //Update record in database
-            }
-            else if (action == "insert")
-            {
-                //Insert record in database
-            }
-            else
-            {
-                //Delete record in database
-            }
-        }
+        //public ActionResult CrudUpdate([FromBody]EditableOrder value, string action)
+        //{
+        //    if (action == "update")
+        //    {
+        //        //Update record in database
+        //    }
+        //    else if (action == "insert")
+        //    {
+        //        //Insert record in database
+        //    }
+        //    else
+        //    {
+        //        //Delete record in database
+        //    }
+        //}
     }
 }
