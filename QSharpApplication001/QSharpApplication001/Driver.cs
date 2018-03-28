4ebf6f4b597ef2d3e8248562b131530563143330
akavick <akavick@tut.by>
2018-03-28 18:53:54 +0300

    1

---
+++ QSharpApplication001/QSharpApplication001/Driver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QSharpApplication001
+{
+    internal class Driver
+    {
+        private static void Main()
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                // Try initial values
+                Result[] initials = { Result.Zero, Result.One };
+                foreach (var initial in initials)
+                {
+                    
+                    var res = BellTest.Run(sim, 1000, initial).Result;
+                    var (numZeros, numOnes) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                }
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
