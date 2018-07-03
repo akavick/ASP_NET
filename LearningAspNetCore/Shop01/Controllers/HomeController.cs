b78fa2f4b4e96c8fde19a412eeccdd2a7cce17a4
akavick <akavick@tut.by>
2018-07-03 23:27:56 +0300

    1

---
+++ LearningAspNetCore/Shop01/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Shop01.MobileDbContext;
+using Shop01.Models;
+
+
+
+
+
+namespace Shop01.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private MobileContext _db;
+
+
+
+
+
+        public HomeController(MobileContext context)
+        {
+            _db = context;
+        }
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View(_db.Phones.ToList());
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+
+            return View();
+        }
+
+
+
+
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            _db.Orders.Add(order);
+
+            // сохраняем в бд все изменения
+            _db.SaveChanges();
+
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+
+}
---
+++ LearningAspNetCore/Shop01/MobileDbContext/SampleData.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+using Shop01.Models;
+
+
+
+
+
+namespace Shop01.MobileDbContext
+{
+
+    public static class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (context.Phones.Any())
+            {
+                return;
+            }
+
+            context.Phones.AddRange
+            (
+                 new Phone
+                 {
+                     Name = "iPhone 6S",
+                     Company = "Apple",
+                     Price = 600
+                 },
+                 new Phone
+                 {
+                     Name = "Samsung Galaxy Edge",
+                     Company = "Samsung",
+                     Price = 550
+                 },
+                 new Phone
+                 {
+                     Name = "Lumia 950",
+                     Company = "Microsoft",
+                     Price = 500
+                 }
+            );
+
+            context.SaveChanges();
+        }
+    }
+
+}
