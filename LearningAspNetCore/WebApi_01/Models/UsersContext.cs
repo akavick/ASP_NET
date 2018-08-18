94fc6d49c8e29114a6538d745b4c82699c4dd707
akavick <akavick@tut.by>
2018-08-18 19:15:15 +0300

    1

---
+++ LearningAspNetCore/WebApi_01/Models/UsersContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+
+namespace WebApi_01.Models
+{
+
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+
+
+
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+    }
+
+}
