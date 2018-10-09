005e537d32bc2e9e8ad97dd207be49228c1b9afe
akavick <akavick@tut.by>
2018-10-10 00:44:44 +0300

    1

--- SamProject/KnockOutJs/wwwroot/js/Ko3.js
+++ SamProject/KnockOutJs/wwwroot/js/Ko3.js
@@ -481,74 +481,78 @@ function formatCurrency(value)
     return `$${value.toFixed(2)}`;
 }
 
-let f3 = (function ()
+(function ()
 {
-    var CartLine = function ()
-    {
-        var self = this;
-
-        self.category = ko.observable();
-        self.product = ko.observable();
-        self.quantity = ko.observable(1);
-
-        self.subtotal = ko.computed(function ()
+    document
+        .querySelector("body")
+        .addEventListener("scriptsLoaded", function ()
         {
-            return self.product() ? self.product().price * parseInt(`0${self.quantity()}`, 10) : 0;
-        });
-
-        // Whenever the category changes, reset the product selection
-        self.category.subscribe(function ()
-        {
-            self.product(undefined);
-        });
-    };
-
-    var Cart = function ()
-    {
-        // Stores an array of lines, and from these, can work out the grandTotal
-        var self = this;
-
-        self.lines = ko.observableArray([new CartLine()]); // Put one line in by default
-
-        self.grandTotal = ko.computed(function ()
-        {
-            var total = 0;
-
-            $.each(self.lines(), function ()
+            var CartLine = function ()
             {
-                 total += this.subtotal();
-            });
+                var self = this;
 
-            return total;
-        });
+                self.category = ko.observable();
+                self.product = ko.observable();
+                self.quantity = ko.observable(1);
 
-        // Operations
-        self.addLine = function ()
-        {
-             self.lines.push(new CartLine());
-        };
+                self.subtotal = ko.computed(function ()
+                {
+                    return self.product() ? self.product().price * parseInt(`0${self.quantity()}`, 10) : 0;
+                });
 
-        self.removeLine = function (line)
-        {
-             self.lines.remove(line);
-        };
+                // Whenever the category changes, reset the product selection
+                self.category.subscribe(function ()
+                {
+                    self.product(undefined);
+                });
+            };
 
-        self.save = function ()
-        {
-            var dataToSave = $.map(self.lines(), function (line)
+            const Cart = function ()
             {
-                return line.product()
-                           ? {
-                               productName: line.product().name,
-                               quantity: line.quantity()
-                           }
-                           : undefined;
-            });
+                // Stores an array of lines, and from these, can work out the grandTotal
+                var self = this;
 
-            alert(`Could now send this to server: ${JSON.stringify(dataToSave)}`);
-        };
-    };
+                self.lines = ko.observableArray([new CartLine()]); // Put one line in by default
 
-    ko.applyBindings(new Cart(), document.getElementById("Ko03"));
+                self.grandTotal = ko.computed(function ()
+                {
+                    var total = 0;
 
+                    $.each(self.lines(), function ()
+                    {
+                        total += this.subtotal();
+                    });
+
+                    return total;
+                });
+
+                // Operations
+                self.addLine = function ()
+                {
+                    self.lines.push(new CartLine());
+                };
+
+                self.removeLine = function (line)
+                {
+                    self.lines.remove(line);
+                };
+
+                self.save = function ()
+                {
+                    const dataToSave = $.map(self.lines(), function (line)
+                    {
+                        return line.product()
+                            ? {
+                                productName: line.product().name,
+                                quantity: line.quantity()
+                            }
+                            : undefined;
+                    });
+
+                    alert(`Could now send this to server: ${JSON.stringify(dataToSave)}`);
+                };
+            };
+
+            ko.applyBindings(new Cart(), document.getElementById("Ko03"));
+        });
 })();
