9553b68045411485f9b31d9b7742003b2f8e42cf
akavick <akavick@tut.by>
2018-10-12 14:36:02 +0300

    1

---
+++ SamTestHumanReservationSystem/SamTestHumanReservationSystem/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SamTestHumanReservationSystem
+{
+    public class Program
+    {
+
+
+        //public static void Main(string[] args)
+        //{
+        //    var webHost = new WebHostBuilder()
+        //                  .UseKestrel()
+        //                  .UseContentRoot(Directory.GetCurrentDirectory())
+        //                  .ConfigureAppConfiguration((hostingContext, config) =>
+        //                  {
+        //                      //var env = hostingContext.HostingEnvironment;
+        //                      config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //                            //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        //                          ;
+        //                      config.AddEnvironmentVariables();
+        //                  })
+        //                  .ConfigureLogging((hostingContext, logging) =>
+        //                  {
+        //                      logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        //                      logging.AddEventLog();
+        //                      //logging.AddEventSourceLogger();
+        //                      //logging.AddDebug();
+        //                  })
+        //                  .UseStartup<Startup>()
+        //                  .Build();
+
+        //    webHost.Run();
+        //}
+
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .ConfigureLogging((hostingContext, logging) =>
+                          {
+                              logging.AddFilter("System", LogLevel.Information)
+                                     .AddFilter("Microsoft", LogLevel.Information);
+
+                              logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                              logging.AddEventLog();
+                              
+                          })
+                          .UseStartup<Startup>();
+        }
+    }
+}
---
+++ SamTestHumanReservationSystem/SamTestHumanReservationSystem/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+
+
+
+namespace SamTestHumanReservationSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            const string sourceName = "TEST_ASPNET_CORE_EVENTLOG";
+
+            if (!EventLog.SourceExists(sourceName))
+            {
+                EventLog.CreateEventSource(sourceName, "Application");
+            }
+
+            loggerFactory.AddEventSourceLogger();
+
+            loggerFactory.AddEventLog(new Microsoft.Extensions.Logging.EventLog.EventLogSettings
+            {
+                SourceName = sourceName,
+                Filter = (str, level) => level > LogLevel.Debug
+            });
+
+            // создаем объект логгера
+            //var logger = loggerFactory.CreateLogger("RequestInfoLogger");
+            // пишем на консоль информацию
+            //logger.LogInformation("Processing request {0}", context.Request.Path);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await next.Invoke();
+            //});
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
