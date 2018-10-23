ee45a1772c1dce1ad66fcd5272540b80b6552539
akavick <akavick@tut.by>
2018-10-23 18:21:07 +0300

    1

--- SamTestCompleted/SamTestCompleted/Program.cs
+++ SamTestCompleted/SamTestCompleted/Program.cs
@@ -38,25 +38,25 @@ namespace SamTestCompleted
         {
             var webHostBuilder = 
                 WebHost.CreateDefaultBuilder(args)
-                       //.ConfigureLogging((hostingContext, logging) =>
-                       //{
-                       //    var loggingSection = hostingContext.Configuration.GetSection("Logging");
-                       //    var sourceName = loggingSection["SourceName"];
-                       //    var logName = loggingSection["LogName"];
+                       .ConfigureLogging((hostingContext, logging) =>
+                       {
+                           var loggingSection = hostingContext.Configuration.GetSection("Logging");
+                           var sourceName = loggingSection["SourceName"];
+                           var logName = loggingSection["LogName"];
 
-                       //    if (!EventLog.SourceExists(sourceName))
-                       //    {
-                       //        EventLog.CreateEventSource(sourceName, logName);
-                       //    }
+                           if (!EventLog.SourceExists(sourceName))
+                           {
+                               EventLog.CreateEventSource(sourceName, logName);
+                           }
 
-                       //    logging.ClearProviders();
-                       //    logging.AddConfiguration(loggingSection);
-                       //    logging.AddEventLog(new EventLogSettings
-                       //    {
-                       //        SourceName = sourceName,
-                       //        LogName = logName
-                       //    });
-                       //})
+                           logging.ClearProviders();
+                           logging.AddConfiguration(loggingSection);
+                           logging.AddEventLog(new EventLogSettings
+                           {
+                               SourceName = sourceName,
+                               LogName = logName
+                           });
+                       })
                        .UseStartup<Startup>();
 
             return webHostBuilder;
